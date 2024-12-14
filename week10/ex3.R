0dd590d191f62b25ed651a7b6b6c259f6633beb4
Ethan Yarberry <eyarber1@jh.edu>
2024-12-13 20:40:42 -0500

    Updated script

--- week10/ex3.R
+++ week10/ex3.R
@@ -3,8 +3,20 @@ library(tidyverse)
 data = read.csv('/Users/cmdb/qbb2024-answers/week10/output.txt')
 
 ggplot(data, aes(x = factor(Gene), y = nascentRNA)) +
-  geom_violin() +
+  geom_violin(fill = "darkgreen", color = "black") +
   labs(title = "Nascent mRNA expression for knockouts",
        x = "Gene knockout",
-       y = "Nascent mRNA levels") +
-  scale_color_manual(values = c("red", "blue", "green", "yellow"))
+       y = "Nascent mRNA levels")
+
+ggplot(data, aes(x = factor(Gene), y = PCNA)) +
+  geom_violin(fill = "darkblue", color = "black") +
+  labs(title = "PCNA expression for knockouts",
+       x = "Gene knockout",
+       y = "PCNA levels")
+
+ggplot(data, aes(x = factor(Gene), y = ratio)) +
+  geom_violin(fill = "darkred", color = "black") +
+  labs(title = "PCNA expression for knockouts",
+       x = "Gene knockout",
+       y = "log2(RNA/PCNA)")
+
