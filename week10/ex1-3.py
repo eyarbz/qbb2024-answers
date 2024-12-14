ce30f5afa4be9818a03b16c5083a20448ac7546a
Ethan Yarberry <eyarber1@jh.edu>
2024-12-13 20:38:32 -0500

    Finished exercise 3

--- week10/ex1-3.py
+++ week10/ex1-3.py
@@ -18,16 +18,18 @@ genes = ["APEX1", "PIM2", "POLR2B", "SRSF1"]
 fields = ["field0", "field1"]
 stains = ["DAPI", "nascentRNA", "PCNA"]
 
+#print(imageio.v3.imread('/Users/cmdb/qbb2024-answers/week10/APEX1_field0_DAPI.tif').shape)
+
 rgbimg_dict = {}
-j = 0
+
 for gene in genes:
     for field in fields:
         rgbimg = np.zeros((520, 616, 3), np.uint16)
         for i, stain in enumerate(stains):
             img_path = week10_path + gene + "_" + field + "_" + stain + ".tif"
             rgbimg[:, :, i] = imageio.v3.imread(img_path)
-        j += 1
-        rgbimg_dict[f"rgbimg_{j}"] = rgbimg
+        condition = gene + '_' + field
+        rgbimg_dict[condition] = rgbimg
 
 ###############
 #Exercise 2
@@ -70,13 +72,49 @@ for key, value in dapi_labels_filtered_dict.items(): #should this be after first
     maxsize = mean + std
     dapi_labels_filtered_dict2[key] = filter_by_size(value, minsize, maxsize)
 
-#print(dapi_labels_filtered_dict2)
-#plt.imshow(dapi_labels_filtered_dict2["rgbimg_6"])
-#plt.show()
-
 ###############
 #Exercise 3
 ###############
-label_array = dapi_labels_filtered_dict2["rgbimg_1"]
-print(np.where(label_array == 185))
 
+condition_list = []
+for key, value in rgbimg_dict.items():
+    nucleus_list = []
+    label_array = dapi_labels_filtered_dict2[key]
+    
+    for i in range(np.amax(label_array)+1):
+        #skip label = 0 (background)
+        if i == 0:
+            continue
+        #append gene name to the list
+        list = []
+        list.append(key.split('_')[0]) #only include gene name, not field
+        
+        where = np.where(label_array == i)
+        #append nascent RNA mean to the list
+        RNA_values = value[:,:,1]
+        nucleus_i_RNA_mean = np.mean(RNA_values[where])
+        list.append(nucleus_i_RNA_mean)
+        #append PCNA mean to the list
+        PCNA_values = value[:,:,2]
+        nucleus_i_PCNA_mean = np.mean(PCNA_values[where])
+        list.append(nucleus_i_PCNA_mean)
+        #append ratio to the list
+        log_ratio = np.log2(nucleus_i_RNA_mean/nucleus_i_PCNA_mean)
+        list.append(log_ratio)
+        #append the list to the master list
+        nucleus_list.append(list)
+
+    condition_list.append(nucleus_list)
+
+output_path = '/Users/cmdb/qbb2024-answers/week10/output.txt'
+
+with open(output_path, 'w') as file:
+    file.write('Gene,nascentRNA,PCNA,ratio\n')
+    for condition in condition_list:
+        for nucleus in condition:
+            condition = nucleus[0]
+            RNA_mean = nucleus[1]
+            PCNA_mean = nucleus[2]
+            log2_ratio = nucleus[3]
+            line = f"{condition},{RNA_mean},{PCNA_mean},{log2_ratio}\n"
+            file.write(line)
